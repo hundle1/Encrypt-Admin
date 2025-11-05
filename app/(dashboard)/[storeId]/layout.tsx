e08650320fa2ca86f92bda20fdbed9ddb2792bf2
HundleTD <trexbairong@gmail.com>
2025-11-05 07:51:43 +0700

    git last

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,39 +1,39 @@
 import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation"
+import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
 import Navbar from "@/components/navbar";
 import { Header } from "@/components/header";
 
 interface DashboardType {
-    children: React.ReactNode;
-    params: { storeId: string }
+  children: React.ReactNode;
+  params: { storeId: string };
 }
 
-export default async function Dashboard({children, params}: DashboardType) {
-    const { userId } = auth();
+export default async function Dashboard({ children, params }: DashboardType) {
+  const { userId } = auth();
 
-    if (!userId) {
-        redirect('/sign-in')
-    }
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
-    const store = await prismadb?.store.findFirst({
-        where: {
-            id: params.storeId,
-            userId
-        }
-    })
+  const store = await prismadb?.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
 
-    if (!store) {
-        redirect('/');
-    }
+  if (!store) {
+    redirect("/");
+  }
 
-    return (
-            <div className="h-screen flex flex-row ">
-                <Navbar/>
-                <div className="w-full border-l">
-                    <Header/>
-                    {children}
-                </div>
-            </div>
-    )
+  return (
+    <div className="h-screen flex flex-row ">
+      <Navbar />
+      <div className="w-full border-l">
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
 }
